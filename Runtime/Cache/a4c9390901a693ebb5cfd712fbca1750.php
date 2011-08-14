6e019f6901846128f1660e6d51e6db5ca3687c51
Jacobi Nie <nehnre@yahoo.com.cn>
2011-08-14 20:51:37 +0800

    从Myapp转换到www,提交到真实环境测试url重写，结果失败

---
+++ Runtime/Cache/a4c9390901a693ebb5cfd712fbca1750.php
@@ -0,0 +1,405 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="Shortcut icon" href="favicon.ico" />
+	<link rel="Bookmark" href="favicon.ico"> 
+	<link href="images/index.css" rel="stylesheet" type="text/css" media="all" />
+	<title>Nie's Home</title>
+	<script language="JavaScript" src="http://www.google.com/jsapi"></script>
+	<script language="JavaScript">
+		google.load("jquery","1");
+	</script>
+	<script language="JavaScript" src="js/core.js"></script>
+	<script language="JavaScript" src="js/index.js"></script>
+</head>
+<body >
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td height="92" align="center" background="images/index_01.gif"><table width="987" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="27">&nbsp;</td>
+        <td width="134"><a href=""><img src="images/index_04.gif" alt="Nie's Home" border="0" longdesc="http://niehonglei.appspot.com" /></a></td>
+        <td width="58" valign="middle">&nbsp;<img src="images/index_06_2.gif" style="display:none" /></td>
+        <!--分类模板开始-->
+        <td width="79" align="center" background="images/index_06.gif" class="fenlei"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="13%" height="41" rowspan="3">&nbsp;</td>
+            <td width="76%" height="7"></td>
+            <td width="11%" rowspan="3">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="12" align="center" valign="top" class="whiteText"><a href="internet" class="whiteText">互联网</a></td>
+          </tr>
+          <tr>
+            <td height="12"></td>
+          </tr>
+        </table></td>
+        <!--分类模板结束-->
+        <td width="30">&nbsp;</td>
+       <!--分类模板开始-->
+        <td width="79" align="center" background="images/index_06.gif" class="fenlei"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="13%" height="41" rowspan="3">&nbsp;</td>
+            <td width="76%" height="7"></td>
+            <td width="11%" rowspan="3">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="12" align="center" valign="top"><a href="java" class="whiteText">Java</a></td>
+          </tr>
+          <tr>
+            <td height="12"></td>
+          </tr>
+        </table></td>
+        <!--分类模板结束-->
+        <td width="30">&nbsp;</td>
+       <!--分类模板开始-->
+        <td width="79" align="center" background="images/index_06.gif" class="fenlei"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="13%" height="41" rowspan="3">&nbsp;</td>
+            <td width="76%" height="7"></td>
+            <td width="11%" rowspan="3">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="12" align="center" valign="top"><a href="search" class="whiteText">搜索</a></td>
+          </tr>
+          <tr>
+            <td height="12"></td>
+          </tr>
+        </table></td>
+        <!--分类模板结束-->
+        <td width="30">&nbsp;</td>
+       <!--分类模板开始-->
+        <td width="79" align="center" background="images/index_06.gif" class="fenlei"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="13%" height="41" rowspan="3">&nbsp;</td>
+            <td width="76%" height="7"></td>
+            <td width="11%" rowspan="3">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="12" align="center" valign="top"><a href="js" class="whiteText">Js</a></td>
+          </tr>
+          <tr>
+            <td height="12"></td>
+          </tr>
+        </table></td>
+        <!--分类模板结束-->
+        <td width="30">&nbsp;</td>
+       <!--分类模板开始-->
+        <td width="79" align="center" background="images/index_06.gif" class="fenlei"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="13%" height="41" rowspan="3">&nbsp;</td>
+            <td width="76%" height="7"></td>
+            <td width="11%" rowspan="3">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="12" align="center" valign="top"><a href="life" class="whiteText">生活</a></td>
+          </tr>
+          <tr>
+            <td height="12"></td>
+          </tr>
+        </table></td>
+        <!--分类模板结束-->
+        <td>&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="800" align="center" valign="top" bgcolor="#E5E2DD"><table width="987" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="722" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td height="67" background="images/index_11.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td height="33" colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td width="2%" height="34">&nbsp;</td>
+                <td width="95%" align="left"  class="hotText"></td>
+                <td width="3%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+                  
+          <!--文章模板开始-->
+          <tr class="contentmodel">
+            <td height="13" background="images/index_14.gif"></td>
+          </tr>
+          <tr class="contentmodel">
+            <td  background="images/index_16.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td colspan="2">&nbsp;</td>
+                <td width="95%" align="left"><a href="#" class="title"></a></td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="5" colspan="2"></td>
+                <td></td>
+                <td></td>
+              </tr>              
+              <tr>
+                <td colspan="2"></td>
+                <td background="images/line.gif" height="1"></td>
+                <td></td>
+                </tr>
+              <tr>
+                <td height="5" colspan="2"></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td colspan="2">&nbsp;</td>
+                <td align="left" class="basicInfo"></td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td colspan="2">&nbsp;</td>
+                <td align="left">&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td width="2%"><p>&nbsp;</p></td>
+                <td colspan="2" align="left" class="content"></td>
+                <td width="1%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr class="contentmodel">
+            <td height="11"  background="images/index_20.gif"></td>
+          </tr>
+		  <!--文章模板结束-->
+                  
+           <!--分页模板开始-->
+          <tr>
+            <td height="13" background="images/index_14.gif"></td>
+          </tr>
+          <tr>
+            <td  background="images/index_16.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="2%">&nbsp;</td>
+                <td width="97%" align="left" class="pageFoot"></td>
+                <td width="1%">&nbsp;</td>
+              </tr>
+              </table></td>
+          </tr>
+          <tr>
+            <td height="11"  background="images/index_20.gif"></td>
+          </tr>
+		  <!--分页模板结束-->
+                    
+        </table></td>
+        <td align="center" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <!--左边模板开始-->  
+          <tr>
+            <td height="12" background="images/index_18.gif"></td>
+          </tr>
+          <tr>
+            <td  background="images/index_22.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td>&nbsp;</td>
+                <td align="left" class="sideTitle"><a href="rss"><img src="images/sub_rss.gif" alt="订阅" width="16" height="16" border="0" align="absmiddle" /></a>订阅</td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="5"></td>
+                <td></td>
+                <td></td>
+              </tr> 
+              <tr>
+                <td></td>
+                <td background="images/line.gif" height="1"></td>
+                <td></td>
+               </tr> 
+                                           
+              <tr>
+                <td width="6%">&nbsp;</td>
+                <td width="91%" align="left">跟随我的twitter<a href="http://twitter.com/nehnre"><img src="images/sub_twitter.gif" alt="twitter" width="16" height="16" border="0" align="absmiddle" /></a><br />跟随我的新浪微博
+                  <a href="http://t.sina.com.cn/nehnre"><img src="images/sina_t.gif" alt="新浪微博" width="16" height="16" border="0" align="absmiddle" /></a><br />跟随我的嘀咕
+                  <a href="http://digu.com/nehnre"><img src="images/digu_t.gif" alt="嘀咕" width="16" height="16" border="0" align="absmiddle" /></a></td>
+                <td width="3%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td height="12"  background="images/index_24.gif"></td>
+          </tr>
+          <!--左边模板结束-->  
+          <!--左边模板开始-->  
+          <tr>
+            <td height="12" background="images/index_18.gif"></td>
+          </tr>
+          <tr>
+            <td  background="images/index_22.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="6%">&nbsp;</td>
+                <td align="left" width="91%">
+					<form action="http://www.google.com/cse" id="cse-search-box">
+					  <div>
+						<input type="hidden" name="cx" value="007282536651609248827:t79dpyawyjs" />
+						<input type="hidden" name="ie" value="UTF-8" />
+						<input type="text" name="q" size="20"/>
+						<input type="submit" name="sa" value="搜索" />
+					  </div>
+					</form>
+					<script type="text/javascript" src="http://www.google.com/cse/brand?form=cse-search-box&lang=zh-Hans"></script>						
+				</td>
+                <td width="3%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td height="12"  background="images/index_24.gif"></td>
+          </tr>
+          <!--左边模板结束-->  
+          <!--左边模板开始-->  
+          <tr>
+            <td height="12" background="images/index_18.gif"></td>
+          </tr>
+          <tr>
+            <td  background="images/index_22.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td>&nbsp;</td>
+                <td align="left" class="sideTitle">★关于本站</td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="5"></td>
+                <td></td>
+                <td></td>
+              </tr> 
+              <tr>
+                <td></td>
+                <td background="images/line.gif" height="1"></td>
+                <td></td>
+               </tr> 
+                                           
+              <tr>
+                <td width="6%">&nbsp;</td>
+                <td width="91%" align="left" class="about"></td>
+                <td width="3%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td height="12"  background="images/index_24.gif"></td>
+          </tr>
+          <!--左边模板结束-->  
+          <!--左边模板开始-->  
+          <tr>
+            <td height="12" background="images/index_18.gif"></td>
+          </tr>
+          <tr>
+            <td  background="images/index_22.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td>&nbsp;</td>
+                <td align="left" class="sideTitle">★最新评论</td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="5"></td>
+                <td></td>
+                <td></td>
+              </tr> 
+              <tr>
+                <td></td>
+                <td background="images/line.gif" height="1"></td>
+                <td></td>
+               </tr> 
+                                            
+              <tr>
+                <td width="6%">&nbsp;</td>
+                <td width="91%" align="left" class="recentComments"></td>
+                <td width="3%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td height="12"  background="images/index_24.gif"></td>
+          </tr>
+          <!--左边模板结束-->                       
+          <!--左边模板开始-->  
+          <tr>
+            <td height="12" background="images/index_18.gif"></td>
+          </tr>
+          <tr>
+            <td  background="images/index_22.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td>&nbsp;</td>
+                <td align="left" class="sideTitle">★友情链接</td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="5"></td>
+                <td></td>
+                <td></td>
+              </tr> 
+              <tr>
+                <td></td>
+                <td background="images/line.gif" height="1"></td>
+                <td></td>
+               </tr> 
+                                           
+              <tr>
+                <td width="6%">&nbsp;</td>
+                <td width="91%" align="left" class="blogroll"></td>
+                <td width="3%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td height="12"  background="images/index_24.gif"></td>
+          </tr>
+          <!--左边模板结束-->  
+          <!--左边模板开始-->  
+          <tr>
+            <td height="12" background="images/index_18.gif"></td>
+          </tr>
+          <tr>
+            <td  background="images/index_22.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td>&nbsp;</td>
+                <td align="left" class="sideTitle">★阅读排行</td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="5"></td>
+                <td></td>
+                <td></td>
+              </tr> 
+              <tr>
+                <td></td>
+                <td background="images/line.gif" height="1"></td>
+                <td></td>
+               </tr> 
+                                            
+              <tr>
+                <td width="6%">&nbsp;</td>
+                <td width="91%" align="left" class="readtop10"></td>
+                <td width="3%">&nbsp;</td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td height="12"  background="images/index_24.gif"></td>
+          </tr>
+          <!--左边模板结束-->                              
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="71" valign="top" background="images/index_30.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="center" class="whiteText">Powered By nehnre | 转载请注明出处 | 管理员登录</td>
+      </tr>
+    </table></td>
+  </tr>  
+</table>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 	// 定义ThinkPHP框架路径   
 	define('THINK_PATH', './ThinkPHP/');   
 
-	//定义项目名称和路径   
-	define('APP_NAME', 'Myapp');   
-	define('APP_PATH', '.');
 
 	// 加载框架入口文件    
 	require(THINK_PATH."/ThinkPHP.php");
